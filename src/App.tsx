b1f207615b7d6035cbf038dbaeb77d06f49f9a4a
Zack Jones <zajonesck83@gmail.com>
2022-12-24 13:59:56 -0600

    added some logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
 import React from "react";
-import "./App.css";
+import ReactDOM from "react-dom/client";
+import "./app.css";
+import App from ".";
+import reportWebVitals from "./reportWebVitals";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="Digital Clock">Learn React</header>
-    </div>
-  );
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+function showTime() {
+  var date = new Date();
+  var hour = date.getHours();
+  var minutes = date.getMinutes();
+  var seconds = date.getSeconds();
+  var session = "AM";
+
+  if (hour == 0) {
+    hour = 12;
+  }
+
+  if (hour > 12) {
+    hour = hour - 12;
+    session = "PM";
+  }
+
+  hour = hour < 10 ? "0" + hour : hour;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + s : s;
+
+  var time = hour + ":" + minutes + ":" + seconds + " " + session;
+  document.getElementById("MyClockDisplay").innerText = time;
+  document.getElementById("MyClockDisplay").textContent = time;
+
+  setTimeout(showTime, 1000);
 }
 
-export default App;
+showTime();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
